2972920cf1735cfa40606a9d24d762e5e9b479d7
liuzhiyu <zhiyu.liu@hexasino.com>
2024-06-11 18:44:05 +0800

    feat:添加热重载

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MenuController } from './menu/menu.controller';
 import { MenuService } from './menu/menu.service';
 import { MenuModule } from './menu/menu.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MenuModule],
+  imports: [MenuModule, UsersModule],
   controllers: [AppController, MenuController],
   providers: [AppService, MenuService],
 })
